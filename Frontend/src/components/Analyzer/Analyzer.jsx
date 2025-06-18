f534451f5eb730cfbc933249ffb9f2bc013b28f9
prathap024-ctrl <prathaplokesh04@gmail.com>
2025-06-18 16:31:32 +0530

    updated

--- Frontend/src/components/Analyzer/Analyzer.jsx
+++ Frontend/src/components/Analyzer/Analyzer.jsx
@@ -38,7 +38,7 @@ export default function ResumeATSReport() {
     try {
       setRefreshing(true);
       const res = await axios.get(
-        `https://craftmycv-vc78.onrender.com/api/analysis/fetch-analysis?ts=${Date.now()}`
+        `https://craftmycv-vc78.onrender.com/api/analysis/fetch-analysis`
       );
 
       if (res.data?.sections) {
